0ead5ba996f25562582bad717f5642978ef9dfec
Oscar Hoang <oscar.hoang@gmail.com>
2021-08-14 00:15:11 -0600

    refactor(App): Scaffold auth0/react-router-dom + switched fr TS to JS

---
+++ packages/spa/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Router, Route, Switch } from 'react-router-dom';
+import { Auth0Provider, withAuthenticationRequired } from '@auth0/auth0-react';
+import { createBrowserHistory } from 'history';
+
+import SiteNav from './components/SiteNav';
+import PublicQuotes from './components/PublicQuotes';
+import User from './components/User';
+
+export const history = createBrowserHistory();
+
+const onRedirectCallback = (appState) => {
+  // Use the router's history module to replace the url
+  history.replace(appState?.returnTo || window.location.pathname);
+};
+
+const ProtectedRoute = ({ component, ...args }) => (
+  <Route component={withAuthenticationRequired(component)} {...args} />
+);
+
+ProtectedRoute.propTypes = {
+  component: PropTypes.any
+};
+
+const App = () => {
+  return (
+    <Auth0Provider
+      domain='dev-v5k0yq0h.us.auth0.com'
+      clientId='b8dxjf5g5lfHhRfyksO63Ct2t522TEvB'
+      redirectUri={window.location.origin}
+      onRedirect={onRedirectCallback}
+    >
+      <Router history={history}>
+        <SiteNav />
+        <Switch>
+          <Route path='/' exact component={PublicQuotes} />
+          <ProtectedRoute path='/user' component={User} />
+        </Switch>
+      </Router>
+    </Auth0Provider>
+  );
+};
+
+export default App;
---
+++ packages/spa/src/components/PublicQuotes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PublicQuotes = () => {
+  return (
+    <div>Public Quotes</div>
+  );
+};
+
+export default PublicQuotes;
