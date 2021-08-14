757b5ee787e0fed434f05e3c6da212fbed95f6cd
Oscar Hoang <oscar.hoang@gmail.com>
2021-08-14 10:07:57 -0600

    style(nav): Update SiteNav UI

--- packages/spa/src/components/SiteNav.js
+++ packages/spa/src/components/SiteNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
 
 const SiteNav = () => {
   const {
@@ -11,24 +12,32 @@ const SiteNav = () => {
     logout,
   } = useAuth0();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-
-  if (isAuthenticated) {
-    return (
-      <div>
-        Hello {user.name}{' '}
-        <button onClick={() => logout({ returnTo: window.location.origin })}>
-          Log out
-        </button>
+  return (
+    <nav className='flex items-center justify-between bg-red-500 p-6'>
+      <div className='flex flex-grow-1'>
+        <Link to='/' className='font-semibold text-xl text-white tracking-tight'>Quotes App</Link>
       </div>
-    );
-  }
-  return <button onClick={loginWithRedirect}>Log in</button>;
+      <React.Fragment>
+        {error && <div className='text-white'>{error.message}</div>}
+        {isLoading && <div className='text-white'>Loading...</div>}
+        {!isLoading && isAuthenticated && (
+          <div className='flex items-center'>
+            <Link to='/user'>
+              <img
+                className='inline object-cover w-8 h-8 mr-2 rounded-full'
+                src={user.picture}
+                alt='Profile image'
+              />
+            </Link>
+            <button className='text-white' onClick={() => logout({ returnTo: window.location.origin })}>
+              Log out
+            </button>
+          </div>
+        )}
+        {!isLoading && !isAuthenticated && <button className='text-white' onClick={loginWithRedirect}>Log in</button>}
+      </React.Fragment>
+    </nav>
+  );
 };
 
 export default SiteNav;
