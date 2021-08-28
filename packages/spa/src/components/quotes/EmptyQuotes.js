b517b46e6c7d97f486a153a66b7fc9a4e9087fdd
Oscar from HustleBox <oscar@hustlebox.ca>
2021-08-27 21:40:08 -0600

    fix(emptyState): Adjust verbiage

--- packages/spa/src/components/quotes/EmptyQuotes.js
+++ packages/spa/src/components/quotes/EmptyQuotes.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const EmptyQuotes = () => (
-  <div className='flex items-center justify-center mx-auto h-screen'>No Quotes Found</div>
+  <div className='flex flex-col items-center justify-center mx-auto h-screen'>
+    <p className='font-normal text-center'>
+      Public Quotes currently unavailable. <br/>
+      Please Login to test CRUD functionality.
+    </p>
+  </div>
 );
 
 export default EmptyQuotes;
